0a67d7a563cc41ced09d1a00708fcb466a8fa7bb
Diogo Ferraz <57525336+Diogo-Ferraz@users.noreply.github.com>
2025-08-31 18:39:54 +0100

    fix: adjust homepage margins to fill entire view point

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -14,11 +14,11 @@ const HomeDiv = styled.div`
     width: 100%;
     height: 100%;
     min-height: 100vh;
-    min-width: 100vw;
     display: flex;
     background: linear-gradient(112.1deg, rgb(32, 38, 57) 11.4%, rgb(63, 76, 119) 70.2%);
     flex-direction: column;
     padding-top: 50px;
+    padding-bottom: 50px;
     align-content: center;
     align-items: center;
 `;
